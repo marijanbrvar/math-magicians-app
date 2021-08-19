db3f9ac036bfd8bb0edc5cb6d7f94bff9a7a3a2f
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-19 17:55:53 +0200

    Refactor Calculator component to Arrow function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,30 +1,24 @@
 /* eslint-disable react/button-has-type */
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import BonusList from './ButtonList';
 import Display from './Display';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Calculator = (props) => {
+  const { display, click } = props;
 
-  render() {
-    const { display, click } = this.props;
-    return (
-      <>
-        <div className="calc">
-          <Display display={display} />
-          <BonusList handleClick={click} />
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className="calc">
+        <Display display={display} />
+        <BonusList handleClick={click} />
+      </div>
+    </>
+  );
+};
 
 Calculator.propTypes = {
   display: PropTypes.string.isRequired,
   click: PropTypes.func.isRequired,
 };
+
+export default Calculator;
