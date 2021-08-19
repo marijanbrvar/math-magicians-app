fea82570e5f743e2f9ba1da7af64208865f8c68b
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-19 17:19:23 +0200

    Refactor App component to Arrow function

--- src/app.js
+++ src/app.js
@@ -1,32 +1,30 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Calculator from './components/calculator';
 import calculate from './logic/calculate';
 import './app.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const App = () => {
+  const [state, setState] = useState(
+    {
       total: null,
       next: null,
       operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+    },
+  );
 
-  handleClick(btnName) {
-    const { total, next, operation } = this.state;
-    this.setState(() => calculate({ total, next, operation }, btnName));
-  }
+  const handleClick = (btnName) => {
+    const newState = calculate(state, btnName);
+    setState((prevState) => ({
+      ...prevState,
+      ...newState,
+    }));
+  };
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <Calculator
-        click={this.handleClick}
-        display={next || total || '0'}
-      />
-    );
-  }
-}
+  return (
+    <Calculator
+      click={handleClick}
+      display={state.next || state.total || '0'}
+    />
+  );
+};
 export default App;
