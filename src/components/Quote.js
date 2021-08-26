28d7fc1de0a87c7ef622c59c86ddad3621a24ef4
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-26 23:34:44 +0200

    Add quote and home components with styles

---
+++ src/components/Quote.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable max-len */
+import React from 'react';
+
+const Quote = () => (
+  <div className="home-container">
+    <h1>The Best Math Quotes</h1>
+    <ul>
+      <li>
+        <p>"Math is the tool specially suited for dealing with abstract concepts of any kind and there is no limit to its power in this field."</p>
+        <i>– Paul Dirac.</i>
+      </li>
+      <li>
+        <p>
+          "There are two ways to do great mathematics. The first is to be smarter than everybody else. The second way is to be stupider than everybody else — but persistent."
+        </p>
+        <i>— Raoul Bott.</i>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Quote;
